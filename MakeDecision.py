1f10af0fce966963d122c7e9cc417517598a7636
Nick Fiete <nicky.fiete@gmail.com>
2017-05-25 12:40:58 -0500

    Add files via upload

--- MakeDecision.py
+++ MakeDecision.py
@@ -30,18 +30,20 @@ class nickNet():
         return 1/(1+np.exp(-num))
     def netRun(self):
         final = []
-        for k in range(X.size):
-            result = np.dot(X[k],W1)
-            result = self.activate(result)
-            for k in rnage(Wmid.size()):
-                result = np.dot(result, wMid(k))
-                result = self.activate(result)
-            result = np.dot(result, self.wfinal)
-            result = activate(result)
+        for k in range(self.X.size):
+            result = net(self.X[k])
             final.append(result)
         return final
     def net(self, var):
-        return 7
+        result = np,dot(var, self.W1)
+        result = self.ativate(result)
+        for k in range(Wmid.size()):
+            result = np.dot(result, wMid(k))
+            result = self.activate(result)
+        result = np.dot(result, self.wfinal)
+        result = activate(result)
+        result = round(result)
+        return result
     def cost(self):
         out = netRun(X)#Make a method that runs every input
         for k in range(len(out)):
@@ -89,17 +91,54 @@ class VarEliminator():
 #nums  = [0,1,2,3,4,5,||||6,7,8,9,10,11,12,13,14,15,16,17]
 
 def playGame(NN):
-    inputs =['u', 'd', 'l', 'r', 'a', 'b', 'x', 'y', 'q', 'p', 'ud', 'ul', 'ur', 'ua', 'ub', 'ux', 'uy', 'uq', 'up', 'dl', 'dr', 'da', 'db', 'dx', 'dy', 'dq', 'dp', 'lr', 'la', 'lb', 'lx', 'ly', 'lq', 'lp', 'ra', 'rb', 'rx', 'ry', 'rq', 'rp', 'ab', 'ax', 'ay', 'aq', 'ap', 'bx', 'by', 'bq', 'bp', 'xy', 'xq', 'xp', 'yq', 'yp', 'qp', 'udl', 'udr', 'uda', 'udb', 'udx', 'udy', 'udq', 'udp', 'ulr', 'ula', 'ulb', 'ulx', 'uly', 'ulq', 'ulp', 'ura', 'urb', 'urx', 'ury', 'urq', 'urp', 'uab', 'uax', 'uay', 'uaq', 'uap', 'ubx', 'uby', 'ubq', 'ubp', 'uxy', 'uxq', 'uxp', 'uyq', 'uyp', 'uqp', 'dlr', 'dla', 'dlb', 'dlx', 'dly', 'dlq', 'dlp', 'dra', 'drb', 'drx', 'dry', 'drq', 'drp', 'dab', 'dax', 'day', 'daq', 'dap', 'dbx', 'dby', 'dbq', 'dbp', 'dxy', 'dxq', 'dxp', 'dyq', 'dyp', 'dqp', 'lra', 'lrb', 'lrx', 'lry', 'lrq', 'lrp', 'lab', 'lax', 'lay', 'laq', 'lap', 'lbx', 'lby', 'lbq', 'lbp', 'lxy', 'lxq', 'lxp', 'lyq', 'lyp', 'lqp', 'rab', 'rax', 'ray', 'raq', 'rap', 'rbx', 'rby', 'rbq', 'rbp', 'rxy', 'rxq', 'rxp', 'ryq', 'ryp', 'rqp', 'abx', 'aby', 'abq', 'abp', 'axy', 'axq', 'axp', 'ayq', 'ayp', 'aqp', 'bxy', 'bxq', 'bxp', 'byq', 'byp', 'bqp', 'xyq', 'xyp', 'xqp', 'yqp']
+    buttons = ['u','d','l', 'r','a','b','x','y','q','p']
+    inputs = []
+    for k in range(len(buttons)):
+        inputs.append(buttons[k])
+    for k in range(len(buttons)):
+        for i in range(k+1,len(buttons)):
+            inputs.append(buttons[k] +buttons[i])
+        
+    for k in range(len(buttons)):
+        for i in range(k+1, len(buttons)):
+            for j in range(i + 1, len(buttons)):
+                inputs.append(buttons[k] + buttons[i] + buttons[j])
     ramCommand = pd.readcsv('isThereANewFrame.csv')
     command = ramCommand.values
     while(True):
-        if(not np.array_equal(pd.readscv('isThereANewFrame.csv').value, command):
-           command = pd.readscv('isThereANewFrame.csv').vsalue
-           Mario = NN.net(command[command.size-1])
+        if(not np.array_equal(pd.readscv('isThereANewFrame.csv').value, command)):
+            command = pd.readscv('isThereANewFrame.csv').values
+            Mario = NN.net(command[command.size-1])
+            command = inputs[Mario]
+            write = [0,0,0,0,0,0,0,0,0,0]
+            for k in range(len(Command)):
+                if(command[k] == 'u'):
+                    write[0] = 1
+                elif(command[k] == 'd'):
+                    write[1] = 1
+                elif(command[k] == 'l'):
+                    write[2] = 1
+                elif(command[k] == 'r'):
+                    write[3] = 1
+                elif(command[k] == 'a'):
+                    write[4] = 1
+                elif(command[k] == 'b'):
+                    write[5] = 1
+                elif(command[k] == 'x'):
+                    write[6] = 1
+                elif(command[k] == 'y'):
+                    write[7] = 1
+                elif(command[k] == 'q'):
+                    write[8] = 1
+                elif(command[k] == 'p'):
+                    write[9] = 1
+            
            
-
-
-    
+                   
+               
+           
+           
+           
     # [up, down, left, right, a, b, x, y, rb, lb] [0 = off, 1 = on]
          
 def main():  
