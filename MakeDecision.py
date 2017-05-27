ebbf2fbdafbfb9259c5f18a2a4db38e705b7a5bc
Nick Fiete <nicky.fiete@gmail.com>
2017-05-27 17:58:58 -0500

    Add files via upload

--- MakeDecision.py
+++ MakeDecision.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as od
+import pandas as pd
 import tensorflow as tf
 import random
 import math
@@ -7,55 +7,58 @@ import csv
 import os
 import sklearn
 from autograd import grad
-import eytan #as WhyIsThisGoddamnLuaPackageNotWorking 
 #Eytan will write some code and I will use the stuff he writes under the eytan package
 class nickNet():
     def __init__(self, X, Y, layers, hiddenSize):
         self.X = X
         self.Y = Y
-        self.input = X[0].size()
+        self.input = X[0].size
         self.output = 1
         self.layers = layers
         self.hidden = hiddenSize
         self.W1 = np.random.randn(self.input, self.hidden)
         self.Wfinal = np.random.randn(self.hidden,self.output)
-        self.Wmid = np.array((), dtpye = float)
+        self.Wmid = np.array((), dtype = float)
+        self. wAll = []
         for k in range(layers - 1):
             if (k%2 != 0):
-                np.append(Wmid,wfinal.T)
+                np.append(self.Wmid,self.wfinal.T)
             else:
-                np.append(Wmid,wfinal)
-       
+                np.append(self.Wmid,self.wfinal)
+        self.wAll.append(self.W1)
+        self.wAll.append(self.Wmid)
+        self.wAll.append(self.Wfinal)
+        self.costPrime = grad(self.cost())
+
     def activate(self, num):
         return 1/(1+np.exp(-num))
     def netRun(self):
         final = []
-        for k in range(self.X.size):
-            result = net(self.X[k])
+        for k in range(self.X.shape[0]):
+            result = self.net(self.X[k])
             final.append(result)
         return final
     def net(self, var):
-        result = np,dot(var, self.W1)
-        result = self.ativate(result)
-        for k in range(Wmid.size()):
-            result = np.dot(result, wMid(k))
+        result = np.dot(var, self.W1)
+        result = self.activate(result)
+        for k in range(self.Wmid.shape[0]):
+            result = np.dot(result, self.wMid[k])
             result = self.activate(result)
-        result = np.dot(result, self.wfinal)
-        result = activate(result)
+        result = np.dot(result, self.Wfinal)
+        result = self.activate(result[0])
         result = round(result)
         return result
     def cost(self):
-        out = netRun(X)#Make a method that runs every input
+        tot = 0;
+        out = self.netRun()#Make a method that runs every input
         for k in range(len(out)):
-            tot+= (out[k]-Y[k])**2
+            tot+= (out[k]-self.Y[k])**2
         tot*=.5
         return tot
-    def costPrime(self):
-        return grad(self.cost())
-    def mutate():
+    def mutate(self):
         self.W1 += self.W1*.01*random.choice((-1,1))
-        for k in range(Wmid.size()):
-            Wmid[k] += self.Wmid*.01*random.choice((-1,1))
+        for k in range(self.Wmid.shape[0]):
+            self.Wmid[k] += self.Wmid*.01*random.choice((-1,1))
         self.Wfinal += self.Wfinal*.01*random.choice((-1,1))
         
 class Trainer():
@@ -76,7 +79,7 @@ class VarEliminator():
         self.Result = NN.netRun(X)
     def extranious(self):
         k = 0
-        while(k < X.size()):
+        while(k < self.X.shape[0]):
             tempX = self.X.delete(k)
             newNet = nickNet(tempX, self.Y, self.NN.layers, self.NN.hidden)
             Train = Trainer(newNet, 1)
@@ -105,10 +108,11 @@ def playGame(NN):
                 inputs.append(buttons[k] + buttons[i] + buttons[j])
     ramCommand = pd.readcsv('isThereANewFrame.csv')
     command = ramCommand.values
-    while(True):
+    int timer = 0
+    while(timer < 100):
         if(not np.array_equal(pd.readscv('isThereANewFrame.csv').value, command)):
             command = pd.readscv('isThereANewFrame.csv').values
-            Mario = NN.net(command[command.size-1])
+            Mario = NN.net(command[0:command.size-1])
             command = inputs[Mario]
             write = [0,0,0,0,0,0,0,0,0,0]
             for k in range(len(Command)):
@@ -132,6 +136,12 @@ def playGame(NN):
                     write[8] = 1
                 elif(command[k] == 'p'):
                     write[9] = 1
+            with open('heyEytan.csv', 'w') as csvfile:
+                for k in write:
+                    print ','.join(k)
+        timer+=1
+        return command[command.size-1]
+                    
             
            
                    
@@ -142,7 +152,14 @@ def playGame(NN):
     # [up, down, left, right, a, b, x, y, rb, lb] [0 = off, 1 = on]
          
 def main():  
-    data_df = pd.readcsv('ramValuesAndInputs.csv')
+    X = np.array(([1, 4, 5], [3,6,3], [6,8,5], [4,8,9], [2,5,3]), dtype = float)
+    Y = np.array([1,2,5,8,2], dtype = float)
+    NN = nickNet(X,Y,1,3)
+    print(NN.costPrime())
+    NN.mutate()
+                 
+    
+    data_df = pd.read_csv('ramValuesAndInputs.csv')
     data = data_df.values
     #Eliminate all values which don't change much
     for k in range(data.size()):
@@ -169,4 +186,5 @@ def main():
         marios.append(Best)
         for i in range():
             marios.append(clone.deepclone(Best.mutate()))
+    
 main()
